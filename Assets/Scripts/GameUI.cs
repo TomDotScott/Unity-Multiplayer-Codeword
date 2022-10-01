4e4bbe8996e231f132e557f79e86ffc50f6350a1
Tom Scott <tom.scott45456@gmail.com>
2022-10-02 00:18:19 +0100

    UI Animations
    Some satisfying wipes between different UI screens were animated and hooked up using the Animator Component

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -7,45 +7,66 @@ public class GameUI : MonoBehaviour
 {
     public static GameUI Instance { get; private set; }
 
-    [SerializeField] private TMPro.TMP_InputField m_InputField;
+    [SerializeField] private TMPro.TMP_InputField m_inputField;
 
-    void Start()
+    [SerializeField] private Animator m_animator;
+
+    // Will be passed into the GameManager and Server to determine which logic the game should follow
+    public enum eGameMode
+    {
+        eNone,
+        eSinglePlayer,
+        eMultiplayer,
+        eMultiplayerVersus
+    }
+
+    // TODO: Use this value to determine which logic for the codeword game to follow
+    public eGameMode GameMode = eGameMode.eNone;
+
+
+void Start()
     {
         Instance = this;
     }
 
     public void OnSingleplayerPressed()
     {
-        Debug.Log("Singleplayer pressed!");
+        m_animator.SetTrigger("FadeMenus");
+        GameMode = eGameMode.eSinglePlayer;
     }
 
     public void OnMultiplayerPressed()
     {
-        Debug.Log("Multiplayer pressed!");
+        m_animator.SetTrigger("OnlineMenu");
+        GameMode = eGameMode.eMultiplayer;
     }
 
     public void OnMultiplayerVersusPressed()
     {
-        Debug.Log("Multiplayer versus pressed!");
+        m_animator.SetTrigger("OnlineMenu");
+        GameMode = eGameMode.eMultiplayerVersus;
     }
 
     public void OnHostPressed()
     {
+        // TODO: Start hosting TCP/UDP server and wait for people to connect
         Debug.Log("Host pressed!");
+        m_animator.SetTrigger("HostMenu");
     }
 
     public void OnConnectPressed()
     {
+        // TODO: Have some additional logic to read from the IP Input and look for a TCP/UDP connection
         Debug.Log("Connect pressed!");
     }
 
     public void OnOnlineBackPressed()
     {
-        Debug.Log("ONLINE back pressed!");
+        m_animator.SetTrigger("StartMenu");
     }
 
     public void OnHostBackPressed()
     {
-        Debug.Log("HOST back pressed!");
+        m_animator.SetTrigger("HostBack");
     }
 }
